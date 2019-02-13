d34759d241be5065f410969e42016f1a5b9e7198
Robin Betz <robin@robinbetz.com>
2019-02-13 14:11:31 -0800

    Let setuptools figure out include and library directories
    - linking to TCL not necessary since we don't use that code
    - don't have -I/usr/include because this breaks conda-build

--- setup.py
+++ setup.py
@@ -33,9 +33,9 @@ psfgenfiles=[
 
 psfext = Extension('_psfgen',
                    define_macros=[('PSFGENTCLDLL_EXPORTS', '1')],
-                   include_dirs=["/usr/include", "./src"],
-                   libraries=["tcl8.5"],
-                   library_dirs=["/usr/lib"],
+                   include_dirs=["./src"],
+                   libraries=[],
+                   library_dirs=[],
                    sources=psfgenfiles
                   )
 
