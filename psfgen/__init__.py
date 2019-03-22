107c999d5a74222b7bf996116a4b7552198e367f
Robin Betz <robin@robinbetz.com>
2019-03-22 14:56:38 -0700

    Bump version: 1.0.0 → 1.0.1

--- psfgen/__init__.py
+++ psfgen/__init__.py
@@ -1,4 +1,4 @@
 from .psfgen import PsfGen
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __author__ = 'Robin Betz'
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ psfext = Extension('_psfgen',
                   )
 
 setup(name="psfgen",
-      version="1.0.0",
+      version="1.0.1",
       description="Protein structure file generator",
       author="Robin Betz, Justin Gullingsrud and Jim Phillips",
       author_email="robin@robinbetz.com",
